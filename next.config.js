cc2b541f984754cc61f7563c2d3474e71b205474
anattalogic <anattalogic@gmail.com>
2026-07-11 04:00:33 +0630

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,3 +4,14 @@ const withNextra = require('nextra')({
 })
 
 module.exports = withNextra()
+const withNextra = require('nextra')({ 
+  theme: 'nextra-theme-docs', 
+  themeConfig: './theme.config.tsx', 
+}) 
+
+module.exports = withNextra({
+  output: 'export',      // GitHub Pages အတွက် သတ်မှတ်ပေးရမည့် Static Export လိုင်း
+  images: {
+    unoptimized: true,   // GitHub Pages မှာ ပုံတွေ ပျက်မသွားအောင် ထိန်းပေးတဲ့လိုင်း
+  },
+})
